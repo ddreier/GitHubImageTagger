4e9cd5af5d77e100ddb30a7ecb11a8ad45e4ae84
Daniel Dreier <daniel@ddreier.com>
2016-09-24 19:56:36 -0500

    Added Knockout bower package and script tags

--- src/GitHubImageTagger.Web/wwwroot/_references.js
+++ src/GitHubImageTagger.Web/wwwroot/_references.js
@@ -4,4 +4,5 @@
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
+/// <reference path="lib/knockout/dist/knockout.js" />
 /// <reference path="lib/tether/dist/js/tether.js" />
