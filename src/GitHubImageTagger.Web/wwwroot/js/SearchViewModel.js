1ca03663d3a30b5f9e54f40770c46cc4e69a4fb2
Daniel Dreier <daniel@ddreier.com>
2016-09-27 22:56:11 -0500

    More work using knockout on the home/search page. RIP popovers.

--- src/GitHubImageTagger.Web/wwwroot/js/SearchViewModel.js
+++ src/GitHubImageTagger.Web/wwwroot/js/SearchViewModel.js
@@ -3,7 +3,36 @@
     self.imageId = id;
     self.url = url;
     self.tags = ko.observableArray();
+    self.newTags = ko.observable();
+    self.showNewTag = ko.observable(false);
+    self.newTagStatusColor = ko.observable();
+
+    self.deleteTag = function (tag) {
+        console.debug(tag);
+        $.ajax("/api/tags/" + tag.tagId, {
+            type: "DELETE",
+            success: function (data, status, xhr) {
+                self.tags.remove(tag);
+            },
+            error: function (xhr, status, error) {
+                alert("Error " + status + " calling API: " + error);
+            }
+        });
+    };
+
+    self.toggleShowNewTag = function () { self.showNewTag(!self.showNewTag()) };
 }
+
+//ko.bindingHandlers.addTagsPopover = {
+//    init: function (element, valueAccessor, allBindingsAccessor, koViewModel, bindingContext) {
+//        var options = ko.utils.unwrapObservable(valueAccessor());
+//        var template = "<div class='input-group'><input type='text' class='form-control tagBox' placeholder='Enter tags, comma separated.' /><span class='input-group-btn'><button class='btn btn-default' type='button' data-bind='click: alert('hi')'><i class='fa fa-check'></i></button></span></div>";
+//        var defaultOptions = { html: true, content: template };
+//        options = $.extend(true, {}, defaultOptions, options);
+//        var pop = $(element).popover(options);
+//        ko.applyBindingsToDescendants(bindingContext, pop);
+//    }
+//}
 
 function SearchViewModel() {
     var self = this;
@@ -31,7 +60,7 @@ function SearchViewModel() {
                 $("#searchSpinner").hide();
             }
         });
-    }
+    };
 
     self.populateImageTags = function () {
         ko.utils.arrayForEach(self.images(), function (img) {
@@ -41,7 +70,37 @@ function SearchViewModel() {
                 }
             );
         });
-    }
+    };
+
+    self.populateTagsForImage = function (img) {
+        $.getJSON("/api/images/tags/" + img.imageId,
+                function (data) {
+                    img.tags(data);
+                }
+            );
+    };
+
+    self.submitTags = function (img) {
+        //img.editEnabled(false);
+        var data = { tags: img.newTags(), imageId: img.imageId };
+
+        if (data.tags !== "") {
+            $.ajax("/api/tags", {
+                type: "POST",
+                data: data,
+                success: function (data, status, xhr) {
+                    img.newTagStatusColor("limegreen");
+                    self.populateTagsForImage(img);
+                    img.newTags(null);
+                    img.showNewTag(false);
+                },
+                error: function (xhr, status, error) {
+                    console.log("error submitting tags: " + error);
+                    img.newTagStatusColor("red");
+                }
+            });
+        }
+    };
 }
 
 var viewModel = new SearchViewModel();
